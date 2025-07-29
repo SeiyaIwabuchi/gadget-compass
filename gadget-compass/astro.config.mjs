b1048ea3b2257ed37a4604e41c516c03d4d4b85d
Seiya Iwabuchi <0123ook@gmail.com>
2025-07-29 22:19:19 +0900

    Add manifest and service worker for gadget compass PWA
    - Created manifest.json for the gadget compass application, defining app metadata, icons, and display settings.
    - Implemented service worker (sw.js) to handle caching of essential resources and manage fetch events for offline capabilities.

--- gadget-compass/astro.config.mjs
+++ gadget-compass/astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://seiyaiwabuchi.github.io',
+    base: '/gadget-compass/gadget-compass/dist',
+});
